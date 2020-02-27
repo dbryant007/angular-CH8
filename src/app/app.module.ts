164b30baf90fae16d6a0722b0be492a0008f0bcc
dbryant007 <dbryant007@hotmail.com>
2020-02-27 11:38:20 -0700

    5 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
+import { StoreComponent } from '.store/store.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
+  
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" }
+    ])],
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
