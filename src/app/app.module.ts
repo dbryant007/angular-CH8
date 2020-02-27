76d725455e6cd91479585237bf521af0bffc2d97
dbryant007 <dbryant007@hotmail.com>
2020-02-27 11:43:51 -0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
-import { StoreComponent } from '.store/store.component';
+import { StoreComponent } from './store/store.component';
 import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
 import { RouterModule } from '@angular/router';
